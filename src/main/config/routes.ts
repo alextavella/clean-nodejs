5ec5ad67eeb13a226bdb4d85fbc9afb01f4b02bf
alextavella <eu.alextavella@gmail.com>
2020-12-17 14:55:54 -0300

    babel!

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,16 +1,19 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-import { resolve, basename } from 'path'
+import { resolve } from 'path'
 
 export const setupRoutes = (app: Express) => {
   const router = Router()
+  const extensions = 'js|ts'
 
   app.use('/api', router)
 
-  readdirSync(resolve(__dirname, '..', 'routes')).map(async (fileName) => {
-    const filename = basename(`../routes/${fileName}`, '.ts')
-    const innerRouter = Router()
-    router.use(`/${filename}`, innerRouter)
-    ;(await import(`../routes/${fileName}`)).default(innerRouter)
-  })
+  readdirSync(resolve(__dirname, '..', 'routes'))
+    .filter((fileName) => fileName.match(new RegExp(`.*\.(${extensions})$`)))
+    .map(async (fileName) => {
+      const [_, filename] = fileName.match(new RegExp(`(.*)\.(${extensions})$`))
+      const innerRouter = Router()
+      router.use(`/${filename}`, innerRouter)
+      ;(await import(`../routes/${fileName}`)).default(innerRouter)
+    })
 }
