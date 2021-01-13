c016e361e612a3beac8700c4aa64f76dec89293d
alextavella <eu.alextavella@gmail.com>
2021-01-13 13:59:36 -0300

    update tsconfig.json and package.json

--- src/main/routes/rankings.ts
+++ src/main/routes/rankings.ts
@@ -3,5 +3,6 @@ import { makeLoadLastRankingController } from '@/main/factories'
 import { Router } from 'express'
 
 export default (router: Router) => {
+  router.get('/', (_req, res) => res.send('ok'))
   router.get('/last', adaptRoute(makeLoadLastRankingController()))
 }
